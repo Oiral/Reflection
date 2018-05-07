98f477336c4abdf06dc6e2d04ab1d4fb5adc1aed
Oiral <hamish@pageland.co.nz>
2018-05-07 22:04:14 +1200

    1st pass movement
    A firest pass on a basic movement for the player using some draw gizmos to give us some feedback while designing

---
+++ Reflection/Assets/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { North, East, South, West };
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject targetTile;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveTile(Direction.North);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveTile(Direction.South);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveTile(Direction.West);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveTile(Direction.East);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetTile.transform.position, 0.1f);
+
+    }
+
+    void MoveTile(Direction dir)
+    {
+        foreach(GameObject tile in targetTile.GetComponent<TileScript>().connections)
+        {
+            if (CheckDirection(targetTile.transform.position,tile.transform.position) == dir)
+            {
+                targetTile = tile;
+                break;
+            }
+        }
+    }
+
+    private Direction CheckDirection(Vector3 startingPos, Vector3 checkingPos)
+    {
+        if (startingPos.x < checkingPos.x)
+        {
+            return Direction.East;
+        }else if (startingPos.x > checkingPos.x)
+        {
+            return Direction.West;
+        }else if (startingPos.z < checkingPos.z)
+        {
+            return Direction.North;
+        }
+        else if (startingPos.z > checkingPos.z)
+        {
+            return Direction.South;
+        }
+        else
+        {
+            Debug.LogError("Can't find Direction - Defaulting to North");
+            return Direction.North;
+        } 
+            
+    }
+
+
+}
---
+++ Reflection/Assets/TileScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    public List<GameObject> connections;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (connections.Count > 0)
+        {
+            foreach (GameObject neighbor in connections)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(Average(transform.position, neighbor.transform.position), new Vector3(0.5f, 0.5f, 0.5f));
+            }
+        }
+    }
+
+    private Vector3 Average(Vector3 pos1, Vector3 pos2)
+    {
+        return (pos1 + pos2) / 2;
+    }
+
+}
